8a7e8851418539ca507f5fc5ee38858665667bf9
shtsukada <st01251988@gmail.com>
2025-07-25 09:48:53 +0900

    feat(observabilityConfig):CRD、Controller実装(ログ出力+Status反映)

--- cmd/main.go
+++ cmd/main.go
@@ -208,6 +208,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "GrpcBurner")
 		os.Exit(1)
 	}
+	if err := (&controller.ObservabilityConfigReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ObservabilityConfig")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err = (&controller.BurnerJobReconciler{
